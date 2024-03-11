802c3b3ef771c1ba975880a81ff534e775e02542
Aws-star <awsalrahim@yahoo.com>
2024-03-10 22:25:17 -0400

    Major Change

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function hoverIn () {
+	 document.getElementById('heroLine').src = "images/heroLineOrange.png";
+}
+
+function hoverOut () {
+	 document.getElementById('heroLine').src = "images/heroLine.png";
+}
